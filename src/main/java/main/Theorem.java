e416c6426c0f9101b35c84a0365aa49f8e0d57e6
kimbi91 <subscribefan@gmail.com>
2022-04-09 09:28:37 +0200

    theorem done

--- src/main/java/main/Theorem.java
+++ src/main/java/main/Theorem.java
@@ -7,17 +7,32 @@ package main;
 public class Theorem {
 
     /* Milyen primitív adattípusok léteznek a Java-ban? Sorold fel őket! - 1p
-        Ide jön a válaszod...
+        boolean, byte, int, long, double, float
      */
 
 
     /* Mi a különbség a while és a do-while ciklusok között? - 2p
-        Ide jön a válaszod...
+        A while ciklusnál feltételhez kötjük az utasítás-blokk végrehajtását míg a do-while ciklusnál elöbb végrehajtjuk
+        az utasítás-blokkot és csak ezután vizsgáljuk meg a feltételt. Mind a kettő addig fut amig a feltétel igaz.
      */
 
 
     /* Mik a tömbök? (Mire használhatóak? Mikor érdemes használni őket? Hogyan hozhatjuk létre őket?) - 3p
-        Ide jön a válaszod...
+        A tömbök ugyanolyan adattípusú változok tárolására használhatóak, igazából ugyanolyan adattípusú változók gyüjteménye,
+        adatok gyüjteményének tárolására érdemes használni őket.
+
+        létrehozásának módjai:
+
+        adattípus[] tömbNeve;   Ebben az esetben csak deklaráltuk.
+
+        adattípus[] tömbNeve = new adattípus[itt megadhatjuk a tömb méretét];
+
+        adattípus[] tömbNeve = new adattípus[]{a tömb elemei ide kerülnek vesszővel elválasztva};
+
+        adattípus[] tömbNeve = {a tömb elemei ide kerülnek vesszővel elválasztva};
+
      */
+
+
 
 }
